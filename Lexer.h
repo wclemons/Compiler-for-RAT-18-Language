21452d934077c4b71362900dfdb64b084fc93c30
wclemons <wclemons@csu.fullerton.edu>
2019-02-05 21:34:40 -0800

    Update Lexer.h

--- Lexer.h
+++ Lexer.h
@@ -57,8 +57,16 @@ while(!eof)
 
 close file
 */
-const string[13] OPERATORS = "+", "-", "%", "/", "*", "<", ">", "=", "==", "<=", ">=", "!=", ".";
-ruct SymbolEntry{
+const string[13] OPERATORS = ["+", "-", "%", "/", "*", "<", ">", "=", "==", "<=", ">=", "!=", "."];
+const string[87] KEYWORDS = ["while", "if", "else", "for", "do", "alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand", "bitor", "bool",
+				"break", "case", "catch", "char", "char16_t", "char32_t", "class", "compl", "const", "constexpr", "const_cast",
+				"continue", "decltype", "default", "delete", "do", "double", "dynamic_cast", "else", "enum", "explicit", "export",				//35
+				"extern", "false", "float", "for", "friend", "goto", "if", "inline", "int", "long", "mutable", "namespace", "new",
+				"noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected", "public", "register",
+				"reinterpret_cast", "return", "short", "sizeof", "static", "static_assert", "static_cast", "struct", "switch",
+				"template", "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename", "union", "unsigned",
+				"using", "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq"]
+struct SymbolEntry{
 		string tokenType;
     string lexemes;
 };
