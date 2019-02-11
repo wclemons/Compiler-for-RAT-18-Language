2f3b966a60908d5ababff71735c0f83a5ebbaee9
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2019-02-10 19:57:21 -0800

    Update Lexer.h

--- Lexer.h
+++ Lexer.h
@@ -3,69 +3,36 @@
 #include <list>
 #include <cassert>
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
-/*
+const short NUM_OPS = 19;
+const short NUM_SEPS = 11;
+const short NUM_KEYWORDS = 88;
 
-**********************************************************
-		Tokens			Lexemes
-		keyword			while, if, else, for, do, alignas, alignof, and, and_eq, asm, auto, bitand, bitor, bool,
-						break, case, catch, char, char16_t, char32_t, class, compl, const, constexpr, const_cast,
-						continue, decltype, default, delete, do, double, dynamic_cast, else, enum, explicit, export, 
-						extern, false, float, for, friend, goto, if, inline, int, long, mutable, namespace, new,
-						noexcept, not, not_eq, nullptr, operator, or, or_eq, private, protected, public, register, 
-						reinterpret_cast, return, short, sizeof, static, static_assert, static_cast, struct, switch,
-						template, this, thread_local, throw, true, try, typedef, typeid, typename, union, unsigned, 
-						using, virtual, void, volatile, wchar_t, while, xor, xor_eq
-
-		separator		(, ), [, ], {, }, ;, :, \n, \t, space
-
-		identifier		any variable name
-
-		operator		+, -, %, /, *, <, >, =, ==, <=, >=, !=, .
-
-		real			any number (int, float, double, real)
-
-*************************************************************
-*/
-
-/*
-take in a file, read the file in different ways depending on the format, either in a while(!eof) and process 1-2 chars at a time,
-or process 1 line of the file at a time
-*/
+const char OPERATORS[NUM_OPS] = { '+', '-', '%', '&', '/', '*', '<', '>', '=', '==', '<=', '>=', '!=', '.', '&&', '||', '!', '!=', '|' };
+const char SEPARATORS[NUM_SEPS] = { '(', ')', ';', '{', '}', '[', ']', ',', '\"', '\'' , ':'};
+const string KEYWORDS[NUM_KEYWORDS] = { "while", "if", "else", "for", "do", "alignas", "alignof", "and", "and_eq", "asm", "auto",
+                              "bitand", "bitor", "bool","break", "case", "catch", "char", "char16_t", "char32_t", "class",
+                              "compl", "const", "constexpr", "const_cast", "continue", "decltype", "default", "delete",
+                              "do","double", "dynamic_cast", "else", "enum", "explicit", "export","extern", "false", "float",
+                              "for", "friend", "goto", "if", "inline", "int", "long", "mutable", "namespace", "new",
+                              "noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected",
+                              "public", "register", "reinterpret_cast", "return", "short", "sizeof", "static", "static_assert",
+                              "static_cast", "struct", "switch", "template", "this", "thread_local", "throw", "true", "try",
+                              "typedef", "typeid", "typename", "union", "unsigned","using", "virtual", "void", "volatile",
+                              "wchar_t", "while", "xor", "xor_eq" };
 
 
-/*						PROCESSING THE FILE IN A LOOP
-infile
-open file
-vector<string> lexemeHolder
-vector<string> tokenHolder
-while(!eof)
-	process a char/ char[] in the case of a keyword or real
-	switch (fin)
-		case(keyword lexeme)	
-			call keyword_fsm function	//returns if/what valid keyword or not a valid keyword
-		case(seperator lexeme)
-			call seperator_fsm function		//returns if/what valid seperator or not a valid seperator
-		case(identifier lexeme)
-			call identifier_fsm function		//returns if/what valid identifier or not a valid identifier
-		case(operator lexeme)
-			call operator_fsm function		//returns if/what valid operator or not a valid operator
-		case(real lexeme)
-			call real_fsm function		//returns if/what valid real or not a valid real
-		case(default)
-			no lexeme					//throw an error
-
-close file
-*/
-enum TokenTypes { Keyword, Identifier, Separator, Operator, Literal, Real };
-
+const string TOKEN_ARRAY[] = { "keyword", "identifier", "separator", "operator", "literal", "real", "endString" };
+enum TokenTypes {Keyword, Identifier, Separator, Operator, Literal, Real, EndString};
 
 struct Token {
      string tokenType;
      string lexeme;
      int startingIndex;
-     Token(string, string, int);
+     Token(string t, string l, int i) : tokenType(t), lexeme(l), startingIndex(i) {};
 };
 
 class SymbolTable {
@@ -81,12 +48,12 @@ private:
      string sourceCode;
      int indexCursor;
 public:
-     SymbolTable nextToken(); //Parses string to find the next available token
-     bool getSourceCode(string, string&); //Opens and reads file contents into a string
+     Lexer() : sourceCode(""), indexCursor(0) {};
+     Token nextToken(); //Parses string to find the next available token
+     bool getSourceCode(string); //Opens and reads file contents into a string
      bool isKeyword(string);
-     bool isSeparator(string);
+     bool isSeparator(char);
      bool isIdentifier(string);
-     bool isOperator(string);
+     bool isOperator(char);
      bool isReal(string);
-
 };
