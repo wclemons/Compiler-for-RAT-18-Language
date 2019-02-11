d698bf47846f8ea5fef0e9709d9d6f33eb42ca72
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2019-02-10 21:14:58 -0800

    Update DFSM.h

--- DFSM.h
+++ DFSM.h
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-enum IdentifierStates {InitialIState, InvalidIdentifier, ValidIdentifier};
-enum RealStates {InitialRState, BeginSignedNumber, SignedInteger, Integer, BeginNumberWithFractionalPart, NumberWithFractionalPart,
-                  BeginNumberWithExponent, BeginNumberWithSignedExponent, NumberWithExponent, InvalidState};
+enum IdentifierStates { InitialIState, InvalidIdentifier, ValidIdentifier };
+enum RealStates {
+     InitialRState, BeginSignedNumber, Integer, BeginNumberWithFractionalPart, NumberWithFractionalPart,
+     BeginNumberWithExponent, BeginNumberWithSignedExponent, NumberWithExponent, InvalidState
+};
 
 class DFSM {
 protected:
@@ -39,21 +41,27 @@ public:
                //FSM implementation
                switch (currentState) {
 
+               case InvalidIdentifier: //We have entered a dead state
+                    return false; //Don't bother processing any more input
+
                case InitialIState:
                     if (isdigit(currentChar))
+                         currentState = InvalidIdentifier;
+                    else if (!isalpha(currentChar) && currentChar != '_')
                          currentState = InvalidIdentifier;
                     else
                          currentState = ValidIdentifier;
                     break;
 
-               case InvalidIdentifier:
-                    if (i == input.size() - 1) // If invalid identifier and the whole string has been processed...
-                         return false;
-                    break;
-
                case ValidIdentifier:
-                    if (i == input.size() - 1) //If valid identifier and the whole string has been processed...
-                         return true;
+                    if (!isalpha(currentChar) && currentChar != '_' && !isdigit(currentChar)) //If character is not a letter, digit or underscore
+                         currentState = InvalidIdentifier;
+                    else { //Else it is a valid character
+                         if (i == input.size() - 1) //If valid identifier and the whole string has been processed...
+                              return true;
+                         else
+                              break;
+                    }
                     break;
                }
           }
@@ -66,13 +74,13 @@ public:
 class RealDFSM : public DFSM {
 public:
      RealDFSM() {
-          startingState = InitialIState;
+          startingState = InitialRState;
           for (int i = 0; i < 7; i++)
                states.insert(i);
           currentState = startingState;
           finalStates.insert(Integer);
-          finalStates.insert(NumberWithExponent);
           finalStates.insert(NumberWithFractionalPart);
+          finalStates.insert(NumberWithExponent);
      }
 
      virtual bool processInput(string input) {
@@ -80,12 +88,14 @@ public:
           for (int i = 0; i < input.size(); i++) {
                currentChar = input[i];
 
-               
+
                //FSM implementation
                switch (currentState) {
 
+               case InvalidState:
+                    break;
+
                case InitialRState:
-                    
                     if (currentChar == '+' || currentChar == '-')
                          currentState = BeginSignedNumber;
                     else if (isdigit(currentChar))
@@ -98,7 +108,7 @@ public:
 
                case BeginSignedNumber:
                     if (isdigit(currentChar))
-                         currentState = SignedInteger;
+                         currentState = Integer;
                     else if (currentChar == '.')
                          currentState = BeginNumberWithFractionalPart;
                     else
@@ -106,12 +116,52 @@ public:
                     break;
 
                case Integer:
+                    if (isdigit(currentChar))
+                         break;
+                    else if (currentChar == '.')
+                         currentState = BeginNumberWithFractionalPart;
+                    else
+                         currentState = InvalidState;
+                    break;
 
                case BeginNumberWithFractionalPart:
+                    if (isdigit(currentChar))
+                         currentState = NumberWithFractionalPart;
+                    else
+                         currentState = InvalidState;
+                    break;
+
                case NumberWithFractionalPart:
+                    if (isdigit(currentChar))
+                         break;
+                    else if (tolower(currentChar) == 'e')
+                         currentState = BeginNumberWithExponent;
+                    else
+                         currentState = InvalidState;
+                    break;
+
                case BeginNumberWithExponent:
+                    if (isdigit(currentChar))
+                         currentState = NumberWithExponent;
+                    else if (currentChar == '+' || currentChar == '-')
+                         currentState = BeginNumberWithSignedExponent;
+                    else
+                         currentState = InvalidState;
+                    break;
+
                case BeginNumberWithSignedExponent:
+                    if (isdigit(currentChar))
+                         break;
+                    else
+                         currentState = InvalidState;
+                    break;
+
                case NumberWithExponent:
+                    if (isdigit(currentChar))
+                         break;
+                    else
+                         currentState = InvalidState;
+                    break;
                }
 
 
