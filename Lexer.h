40d2de81a7803a6e75b19a79cf30e59aa36c65db
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2019-02-19 21:21:32 -0800

    Update Lexer.h

--- Lexer.h
+++ Lexer.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include <string>
+#include <vector>
 #include <list>
 #include <cassert>
 #include <iostream>
@@ -7,32 +9,48 @@
 
 using namespace std;
 
-const short NUM_OPS = 19;
-const short NUM_SEPS = 11;
-const short NUM_KEYWORDS = 88;
+const short NUM_OPS = 8;
+const short NUM_SEPS = 13;
+const short NUM_KEYWORDS = 15;
 
-const char OPERATORS[NUM_OPS] = { '+', '-', '%', '&', '/', '*', '<', '>', '=', '==', '<=', '>=', '!=', '.', '&&', '||', '!', '!=', '|' };
-const char SEPARATORS[NUM_SEPS] = { '(', ')', ';', '{', '}', '[', ']', ',', '\"', '\'' , ':'};
-const string KEYWORDS[NUM_KEYWORDS] = { "while", "if", "else", "for", "do", "alignas", "alignof", "and", "and_eq", "asm", "auto",
-                              "bitand", "bitor", "bool","break", "case", "catch", "char", "char16_t", "char32_t", "class",
-                              "compl", "const", "constexpr", "const_cast", "continue", "decltype", "default", "delete",
-                              "do","double", "dynamic_cast", "else", "enum", "explicit", "export","extern", "false", "float",
-                              "for", "friend", "goto", "if", "inline", "int", "long", "mutable", "namespace", "new",
-                              "noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected",
-                              "public", "register", "reinterpret_cast", "return", "short", "sizeof", "static", "static_assert",
-                              "static_cast", "struct", "switch", "template", "this", "thread_local", "throw", "true", "try",
-                              "typedef", "typeid", "typename", "union", "unsigned","using", "virtual", "void", "volatile",
-                              "wchar_t", "while", "xor", "xor_eq" };
+const char OPERATORS[NUM_OPS] = { '*', '+', '-', '=', '/', '>', '<', '%'};
+
+const char SEPARATORS[NUM_SEPS] = {'/', '(', ')', '{', '}', '[', ']', ',', '.', ':', ';', '!',' ' };
+
+const string KEYWORDS[NUM_KEYWORDS] = { "int", "float", "bool", "if", "else", "then", "do", "while", 
+                                        "whileend", "do", "doend", "for", "and", "or" , "function" };
 
 
-const string TOKEN_ARRAY[] = { "keyword", "identifier", "separator", "operator", "literal", "real", "endString" };
-enum TokenTypes {Keyword, Identifier, Separator, Operator, Literal, Real, EndString};
+/*		C++ Operators
+{ '+', '-', '%', '&', '/', '*', '<', '>', '=', '==', '<=', '>=', '!=', '.', '&&', '||', '!', '!=', '|', '+=',
+  '-=', '/=', '*=', '%=', ':', '::' };*/
+
+/*        C++ Separators
+{ ' ', '(', ')', ';', '{', '}', '[', ']', ',', '\"', '\'' };*/
+
+/*		C++ keywords
+"while", "if", "else", "for", "do", "alignas", "alignof", "and", "and_eq", "asm", "auto",
+"bitand", "bitor", "bool","break", "case", "catch", "char", "char16_t", "char32_t", "class",
+"compl", "const", "constexpr", "const_cast", "continue", "decltype", "default", "delete",
+"do","double", "dynamic_cast", "else", "enum", "explicit", "export","extern", "false", "float",
+"for", "friend", "goto", "if", "inline", "int", "long", "mutable", "namespace", "new",
+"noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected",
+"public", "register", "reinterpret_cast", "return", "short", "sizeof", "static", "static_assert",
+"static_cast", "struct", "switch", "template", "this", "thread_local", "throw", "true", "try",
+"typedef", "typeid", "typename", "union", "unsigned","using", "virtual", "void", "volatile",
+"wchar_t", "while", "xor", "xor_eq" };*/
+
 
 struct Token {
      string tokenType;
      string lexeme;
-     int startingIndex;
-     Token(string t, string l, int i) : tokenType(t), lexeme(l), startingIndex(i) {};
+     Token() : tokenType(""), lexeme("") {};
+     Token(string t, string l) : tokenType(t), lexeme(l) {};
+     Token(const Token& token) {
+          tokenType = token.tokenType;
+          lexeme = token.lexeme;
+     }
+
 };
 
 class SymbolTable {
@@ -45,15 +63,14 @@ public:
 
 class Lexer {
 private:
-     string sourceCode;
-     int indexCursor;
+     vector<string> sourceCode;
 public:
-     Lexer() : sourceCode(""), indexCursor(0) {};
+     Lexer() : sourceCode() {};
      Token nextToken(); //Parses string to find the next available token
-     bool getSourceCode(string); //Opens and reads file contents into a string
+     bool getSourceCode(string); //Opens and reads file contents into a vector of strings
      bool isKeyword(string);
      bool isSeparator(char);
-     bool isIdentifier(string);
+     bool isIdentifier(string, string&);
      bool isOperator(char);
-     bool isReal(string);
+     bool isReal(string, string&);
 };
