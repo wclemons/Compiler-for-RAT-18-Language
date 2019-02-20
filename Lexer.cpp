476e586998c3b1808edc30e8c9500ac8e3f84c99
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2019-02-19 21:20:46 -0800

    Update Lexer.cpp

--- Lexer.cpp
+++ Lexer.cpp
@@ -4,13 +4,33 @@
 
 bool Lexer::getSourceCode(string fileName) {
      ifstream fileHandler(fileName);
-     if (fileHandler.fail()) {
+     if (fileHandler.fail())
           return false;
-     }
+     
+     char character;
      string temp;
-     while (!fileHandler.eof()) {
-          getline(fileHandler, temp);
-          sourceCode += temp;
+     while (fileHandler.get(character)) {
+          if (character == '!') {
+               while (fileHandler.get(character)) {
+                    if (character == '!')
+                         break;
+               }
+               //if (character != '!') Missing a closing !
+          }
+          else if (character == ' ' || character == '\n') {
+               if (temp.size() == 0)
+                    continue;
+               sourceCode.push_back(temp);
+               temp = "";
+          }
+          else
+               temp += character;
+     }
+
+     //If we've reached the end of the file and temp 
+     //still has characters in it add it to sourceCode vector
+     if (temp.size() != 0) {
+          sourceCode.push_back(temp);
      }
      fileHandler.close();
      return true;
@@ -19,47 +39,109 @@ bool Lexer::getSourceCode(string fileName) {
 
 //Get the next token in the string leaving off from current index
 Token Lexer::nextToken() {
-     if (indexCursor == sourceCode.length()) {
-          return Token(TOKEN_ARRAY[EndString], "\0", indexCursor);
+     if (sourceCode.empty()) {
+          return Token("endOfCode", "");
      }
+     string codeChunk = sourceCode.front(); //Chunk of code to analyze for token(s)
 
-     int tokenStartIndex = indexCursor;
+     //Buffer will be a reference string that is passed to the helper functions/FSMs.
+     //If the helper functions return true, the contents of the buffer (if any)
+     //is a valid token
      string buffer = "";
-     char currentCharacter = sourceCode[indexCursor]; //For readability
-     buffer += currentCharacter;
 
-     if (isalpha(currentCharacter) || currentCharacter == '_') {
-          //Process as potential identifier or keyword
-          //While there is a next available character in the string that is a letter, digit, or underscore...
-          while (indexCursor + 1 <= sourceCode.length() - 1 && ( isalpha(sourceCode[indexCursor + 1]) || isdigit(sourceCode[indexCursor + 1])
-                                                                 || sourceCode[indexCursor + 1] == '_' ))
-          {
-               currentCharacter = sourceCode[indexCursor + 1];
-               buffer += currentCharacter;
-               indexCursor++;
+     //If a possible identifier lies within the chunk of source code, 
+     //the identifier DFSM will identify it and mark it and store it in the buffer.
+     if (isIdentifier(codeChunk, buffer)) {
+          //Get the amount of characters that were used/validated by the FSM
+          string processedSubstring;  //Stores what was processed by the FSM
+          bool processedWholeChunk = false; //Determines if the identifier/keyword is/is not the length of the whole code chunk
+
+          if (codeChunk.size() != buffer.size()) //If it is not the length of the code chunk
+               processedSubstring = codeChunk.substr(0, buffer.size()); //Extract the part that is the identifier/keyword
+          else { //Otherwise, the whole code chunk is an identifier/keyword
+               processedSubstring = codeChunk;
+               processedWholeChunk = true;
           }
-          //Now we should have a string. Let's figure out what it is...
-          if (buffer[0] == '_' && isIdentifier(buffer)) //We know it can't be a keyword if it starts with an '_'. Short circuit evaluation
-               return Token("keyword", buffer, tokenStartIndex);
-          else if (isKeyword(buffer)) //Otherwise, check if it is a keyword first
-               return Token("keyword", buffer, tokenStartIndex);
-          else if (isIdentifier(buffer)) //If not a keyword, check if it is a valid identifier
-               return Token("keyword", buffer, tokenStartIndex);
+
+          //Before we continue, we must remove the part of the code chunk that has already been processed
+          if (!processedWholeChunk) {
+               sourceCode[0] = codeChunk.substr(buffer.size(), codeChunk.size());
+          }
+          else //The entire chunk has been processed, so remove the chunk
+               sourceCode.erase(sourceCode.begin());
+
+          //Now we can return the token
+
+          //If it's a valid identifier, it could be a valid keyword as well. So check...
+          if (isKeyword(processedSubstring))
+               return Token("keyword", processedSubstring);
+          else
+               return Token("identifier", processedSubstring);
+
+     }
+     
+     else if (isOperator(codeChunk[0])) {
+          //Store the operator lexeme
+          string opLexeme = "";
+          opLexeme += codeChunk[0];
+          
+          //Update the code chunk
+          if (codeChunk.size() == 1) {
+               sourceCode.erase(sourceCode.begin());
+          }
           else {
-               cout << "\n\nInvalid identifier\n\n";
-               system("PAUSE");
-          } 
+               sourceCode[0] = codeChunk.substr(1, codeChunk.size());
+          }
+          
+          //Return the token
+          return Token("operator", opLexeme);
      }
-     else if (isdigit(currentCharacter)) {
-          //Process as potential real number
+
+     else if (isSeparator(codeChunk[0])) {
+          //Store the separator lexeme
+          string sepLexeme;
+          sepLexeme += codeChunk[0];
+          
+          //Update the code chunk
+          if (codeChunk.size() == 1) {
+               sourceCode.erase(sourceCode.begin());
+          }
+          else {
+               sourceCode[0] = codeChunk.substr(1, codeChunk.size());
+          }
+
+          //Return the token
+          return Token("separator", sepLexeme);
      }
-     else if (currentCharacter == '\"') {
-          //We may be encountering a string literal
+
+     else if (isReal(codeChunk, buffer)) {
+          //Get the amount of characters that were used/validated by the FSM
+          string processedSubstring;  //Stores what was processed by the FSM
+          bool processedWholeChunk = false; //Determines if the identifier/keyword is/is not the length of the whole code chunk
+
+          if (codeChunk.size() != buffer.size()) //If it is not the length of the code chunk
+               processedSubstring = codeChunk.substr(0, buffer.size()); //Extract the part that is the identifier/keyword
+          else { //Otherwise, the whole code chunk is an real
+               processedSubstring = codeChunk;
+               processedWholeChunk = true;
+          }
+
+          //Before we continue, we must remove the part of the code chunk that has already been processed
+          if (!processedWholeChunk) {
+               sourceCode[0] = codeChunk.substr(buffer.size(), codeChunk.size());
+          }
+          else //The entire chunk has been processed, so remove the chunk
+               sourceCode.erase(sourceCode.begin());
+
+          //Now we can return the token
+          return Token("real", processedSubstring);
+
      }
-     else {
-          cout << "\n\nInvalid input\n\n";
-          system("PAUSE");
-     }
+
+
+     cout << "\n\nCompiler error caused in code chunk: \n" << codeChunk;
+     system("PAUSE");
+     return Token();
 }
 
 bool Lexer::isKeyword(string input) {
@@ -70,13 +152,14 @@ bool Lexer::isKeyword(string input) {
      return false;
 }
 
-bool Lexer::isIdentifier(string input) {
+bool Lexer::isIdentifier(string input, string &buffer) {
      IdentifierDFSM fsm;
-     return fsm.processInput(input);
+     return fsm.processInput(input, buffer);
 }
 
-bool Lexer::isReal(string input) {
-     return true;
+bool Lexer::isReal(string input, string &buffer) {
+     RealDFSM fsm;
+     return fsm.processInput(input, buffer);
 }
 
 bool Lexer::isOperator(char input) {
@@ -108,9 +191,9 @@ void SymbolTable::printTable() {
           cout << "\nNo entries\n";
           return;
      }
-     cout << "    Token     |     Lexeme     |   Starting Index\n";
-     cout << "----------------------------------------------------\n";
+     cout << "    Token     |     Lexeme     |\n";
+     cout << "--------------------------------\n";
      for (list<Token>::iterator p = entries.begin(); p != entries.end(); p++) {
-          cout << left << "  " << setw(12) << p->tokenType << "|  " << setw(14) << p->lexeme << "|  " << p->startingIndex << endl;
+          cout << left << "  " << setw(12) << p->tokenType << "|  " << setw(14) << p->lexeme << "|  " << endl;// << p->startingIndex << endl;
      }
 }
