78d99064e0f1bb0000002482ff13bc3d066a0644
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2019-03-03 16:02:09 -0800

    Update Lexer.h

--- Lexer.h
+++ Lexer.h
@@ -21,26 +21,6 @@ const string KEYWORDS[NUM_KEYWORDS] = { "int", "float", "bool", "if", "else", "t
                                         "whileend", "do", "doend", "for", "and", "or" , "function" };
 
 
-/*		C++ Operators
-{ '+', '-', '%', '&', '/', '*', '<', '>', '=', '==', '<=', '>=', '!=', '.', '&&', '||', '!', '!=', '|', '+=',
-  '-=', '/=', '*=', '%=', ':', '::' };*/
-
-/*        C++ Separators
-{ ' ', '(', ')', ';', '{', '}', '[', ']', ',', '\"', '\'' };*/
-
-/*		C++ keywords
-"while", "if", "else", "for", "do", "alignas", "alignof", "and", "and_eq", "asm", "auto",
-"bitand", "bitor", "bool","break", "case", "catch", "char", "char16_t", "char32_t", "class",
-"compl", "const", "constexpr", "const_cast", "continue", "decltype", "default", "delete",
-"do","double", "dynamic_cast", "else", "enum", "explicit", "export","extern", "false", "float",
-"for", "friend", "goto", "if", "inline", "int", "long", "mutable", "namespace", "new",
-"noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected",
-"public", "register", "reinterpret_cast", "return", "short", "sizeof", "static", "static_assert",
-"static_cast", "struct", "switch", "template", "this", "thread_local", "throw", "true", "try",
-"typedef", "typeid", "typename", "union", "unsigned","using", "virtual", "void", "volatile",
-"wchar_t", "while", "xor", "xor_eq" };*/
-
-
 struct Token {
      string tokenType;
      string lexeme;
