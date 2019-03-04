775aae00ae00ee67571ece3c1e324809bdba4f8f
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2019-03-03 16:00:56 -0800

    Update DFSM.h

--- DFSM.h
+++ DFSM.h
@@ -4,153 +4,107 @@
 
 using namespace std;
 
-enum IdentifierStates { InitialIState, InvalidIdentifier, ValidIdentifier };
+enum IdentifierStates { InitialIState, ValidIdentifier, InvalidIdentifier, TerminateIdentifier };
+enum IdentifierInputs  { letter, $, id_digit, id_other };
 
-enum RealStates { InitialRState, Integer, BeginNumberWithFractionalPart, NumberWithFractionalPart, InvalidReal };
+enum RealStates { InitialRState, Integer, BeginNumberWithFractionalPart, NumberWithFractionalPart, InvalidReal, TerminateReal };
+enum RealInputs { r_digit, period, r_other };
 
-enum IntegerStates { InitialIntState, InvalidInteger, ValidInteger};
+enum IntegerStates { InitialIntState, ValidInteger, InvalidInteger, TerminateInteger };
+enum IntegerInputs { i_digit, i_other };
 
 class DFSM {
 protected:
      int startingState;
-     set<int> states;
      int currentState;
+     set<int> states;
+     set<int> inputs;
      set<int> finalStates;
 public:
      virtual bool processInput(string, string&) = 0;
+     virtual int inputType(char) = 0;
 };
 
 
 
 class IdentifierDFSM : public DFSM {
+private:
+     int transitionTable[4][4];
 public:
      IdentifierDFSM() {
           startingState = InitialIState;
-          for (int i = 0; i < 3; i++)
-               states.insert(i);
           currentState = startingState;
+          for (int i = 0; i < 4; i++)
+               states.insert(i);
+          for (int i = 0; i < 4; i++)
+               inputs.insert(i);
           finalStates.insert(ValidIdentifier);
+          finalStates.insert(TerminateIdentifier);
+
+         //State transition table for an identifier
+         //                      |  0       1      2         3
+         //                      |letter    $    digit     other
+         //   -------------------|------------------------------ 
+         //  0 Starting State    |  1       2      2         2
+         //  1 Valid Identifier  |  1       1      1         3
+         //  2 Invalid Identifier|  2       2      2         2
+         //  3 Terminate Iden.   |  0       0      0         0
+
+          transitionTable[InitialIState][letter] = ValidIdentifier;
+          transitionTable[InitialIState][$] = InvalidIdentifier;
+          transitionTable[InitialIState][id_digit] = InvalidIdentifier;
+          transitionTable[InitialIState][id_other] = InvalidIdentifier;
+          transitionTable[ValidIdentifier][letter] = ValidIdentifier;
+          transitionTable[ValidIdentifier][$] = ValidIdentifier;
+          transitionTable[ValidIdentifier][id_digit] = ValidIdentifier;
+          transitionTable[ValidIdentifier][id_other] = TerminateIdentifier;
+          transitionTable[InvalidIdentifier][letter] = InvalidIdentifier;
+          transitionTable[InvalidIdentifier][$] = InvalidIdentifier;
+          transitionTable[InvalidIdentifier][id_digit] = InvalidIdentifier;
+          transitionTable[InvalidIdentifier][id_other] = InvalidIdentifier;
+          transitionTable[TerminateIdentifier][letter] = InitialIState;
+          transitionTable[TerminateIdentifier][$] = InitialIState;
+          transitionTable[TerminateIdentifier][id_digit] = InitialIState;
+          transitionTable[TerminateIdentifier][id_other] = InitialIState;
      }
 
      //Finite State Machine for validating identifier tokens
      virtual bool processInput(string input, string &buffer) {
           char currentChar;
           for (int i = 0; i < input.size(); i++) {
+               if (currentState == TerminateIdentifier || currentState == InvalidIdentifier)
+                    break;
                currentChar = input[i];
-
-               //FSM implementation
-               switch (currentState) {
-
-               case InvalidIdentifier: //We have entered a dead state
-                    buffer = "";
-                    return false; //Don't bother processing any more input
-
-               case InitialIState:
-                    if (!isalpha(currentChar))
-                         currentState = InvalidIdentifier;
-                    else {
-                         buffer += currentChar;
-                         currentState = ValidIdentifier;
-                    }
-                    break;
-
-               case ValidIdentifier:
-                    if (isalpha(currentChar) || currentChar == '$' || isdigit(currentChar)) { //If character is a letter, $, or digit
-                         buffer += currentChar;
-                         currentState = ValidIdentifier;
-                    }
-                    else                 //Else were encountered a space or some other character
-                         return true;   //Since we are in a valid state, return true
-                    break;
-               }
+               currentState = transitionTable[currentState][inputType(currentChar)];
+               if (currentState == ValidIdentifier)
+                    buffer += currentChar;
           }
-          //We will only reach this point if the input size is 1 character
-          //However, the state will have been correctly set by the fsm
+          //The state should be correctly set by the FSM
           if (currentState == InvalidIdentifier)
                return false;
           else
                return true;
      }
-};
 
-class RealDFSM : public DFSM {
-public:
-     RealDFSM() {
-          startingState = InitialRState;
-          for (int i = 0; i < 5; i++)
-               states.insert(i);
-          currentState = startingState;
-          finalStates.insert(NumberWithFractionalPart);
-     }
-
-
-     virtual bool processInput(string input, string &buffer) {
-          char currentChar;
-          for (int i = 0; i < input.size(); i++) {
-               currentChar = input[i];
-
-
-               //FSM implementation
-			   switch (currentState) {
-
-			   case InvalidReal:
-				   buffer = "";
-				   return false;
-
-			   case InitialRState:
-				   if (isdigit(currentChar)) {
-					   currentState = Integer;
-					   buffer += currentChar;
-				   }
-				   else
-					   currentState = InvalidReal;
-				   break;
-
-			   case Integer:
-				   if (isdigit(currentChar)) {
-					   //Same state as before
-					   buffer += currentChar;
-					   break;
-				   }
-				   else if (currentChar == '.') {
-					   currentState = BeginNumberWithFractionalPart;
-					   buffer += currentChar;
-				   }
-				   else
-					   currentState = InvalidReal;
-				   break;
-
-			   case BeginNumberWithFractionalPart:
-				   if (isdigit(currentChar)) {
-					   currentState = NumberWithFractionalPart;
-					   buffer += currentChar;
-				   }
-				   else
-					   currentState = InvalidReal;
-				   break;
-
-			   case NumberWithFractionalPart:
-                       if (isdigit(currentChar)) {
-                            //Same state as before
-                            buffer += currentChar;
-                            break;
-                       }
-                       else
-                            return true;
-                       break;
-                  }
-          }
-          //We will only reach this point if the input size is 1 character
-          //However, the state will have been correctly set by the fsm
-          if (currentState != NumberWithFractionalPart) {
-               buffer = "";
-               return false;
-          }
+     virtual int inputType(char currentChar) {
+          if (isalpha(currentChar))
+               return letter;
+          else if (currentChar == '$')
+               return $;
+          else if (isdigit(currentChar))
+               return id_digit;
           else
-               return true;
+               return id_other;
      }
 };
+
+
+
+
+
 class IntegerDFSM : public DFSM {
+private:
+     int transitionTable[5][2];
 public:
      IntegerDFSM() {
           startingState = InitialIntState;
@@ -158,48 +112,125 @@ public:
                states.insert(i);
           currentState = startingState;
           finalStates.insert(ValidInteger);
+          finalStates.insert(TerminateInteger);
+
+          //State transition table for an integer
+          //                      |  0        1  
+          //                      |digit    other 
+          //   -------------------|---------------
+          //  0 Starting State    |  1       2    
+          //  1 Valid Integer     |  1       3      
+          //  2 Invalid Integer   |  2       2      
+          //  3 Terminate Integer |  0       0
+
+          transitionTable[InitialIntState][i_digit] = ValidInteger;
+          transitionTable[InitialIntState][i_other] = InvalidInteger;
+          transitionTable[ValidInteger][i_digit] = ValidInteger;
+          transitionTable[ValidInteger][i_other] = TerminateInteger;
+          transitionTable[InvalidInteger][i_digit] = InvalidInteger;
+          transitionTable[InvalidInteger][i_other] = InvalidInteger;
+          transitionTable[TerminateInteger][i_digit] = InitialIntState;
+          transitionTable[TerminateInteger][i_other] = InitialIntState;
      }
 
      virtual bool processInput(string input, string &buffer) {
           char currentChar;
           for (int i = 0; i < input.size(); i++) {
+               if (currentState == TerminateInteger || currentState == InvalidInteger)
+                    break;
                currentChar = input[i];
-
-
-               //FSM implementation
-               switch (currentState) {
-
-               case InvalidInteger:
-                    buffer = "";
-                    return false;
-
-               case InitialIntState:
-                    if (isdigit(currentChar)) {
-                         currentState = ValidInteger;
-                         buffer += currentChar;
-                    }
-                    else
-                         currentState = InvalidInteger;
-                    break;
-
-               case ValidInteger:
-                    if (isdigit(currentChar)) {
-                         //Same state as before
-                         buffer += currentChar;
-                         break;
-                    }
-                    else
-                         return true;
-                    break;
-               }
+               currentState = transitionTable[currentState][inputType(currentChar)];
+               if (currentState == ValidInteger)
+                    buffer += currentChar;
           }
-          //We will only reach this point if the input size is 1 character
-          //However, the state will have been correctly set by the fsm
-          if (currentState == InvalidInteger) {
+          //The state should be correctly set by the FSM
+          if (currentState == InvalidInteger)
+               return false;
+          else
+               return true;
+     }
+
+     virtual int inputType(char currentChar) {
+          if (isdigit(currentChar))
+               return i_digit;
+          else
+               return i_other;
+     }
+};
+
+
+
+
+
+class RealDFSM : public DFSM {
+private:
+     int transitionTable[6][3];
+public:
+     RealDFSM() {
+          startingState = InitialRState;
+          for (int i = 0; i < 5; i++)
+               states.insert(i);
+          currentState = startingState;
+          finalStates.insert(NumberWithFractionalPart);
+          finalStates.insert(TerminateReal);
+          
+          //State transition table for an identifier
+          //                      |  0       1         2
+          //                      |digit   period    other
+          //   -------------------|-------------------------
+          //  0 Starting State    |  1       4        4
+          //  1 Integer           |  1       2        4
+          //  2 Begin # with frac.|  3       4        4
+          //  3 # with frac. part |  3       5        5
+          //  4 Invalid Real      |  4       4        4
+          //  5 Terminate Real    |  0       0        0 
+
+          transitionTable[InitialRState][r_digit] = Integer;
+          transitionTable[InitialRState][period] = InvalidReal;
+          transitionTable[InitialRState][r_other] = InvalidReal;
+          transitionTable[Integer][r_digit] = Integer;
+          transitionTable[Integer][period] = BeginNumberWithFractionalPart;
+          transitionTable[Integer][r_other] = InvalidReal;
+          transitionTable[BeginNumberWithFractionalPart][r_digit] = NumberWithFractionalPart;
+          transitionTable[BeginNumberWithFractionalPart][period] = InvalidReal;
+          transitionTable[BeginNumberWithFractionalPart][r_other] = InvalidReal;
+          transitionTable[NumberWithFractionalPart][r_digit] = NumberWithFractionalPart;
+          transitionTable[NumberWithFractionalPart][period] = TerminateReal;
+          transitionTable[NumberWithFractionalPart][r_other] = TerminateReal;
+          transitionTable[InvalidReal][r_digit] = InvalidReal;
+          transitionTable[InvalidReal][period] = InvalidReal;
+          transitionTable[InvalidReal][r_other] = InvalidReal;
+          transitionTable[TerminateReal][r_digit] = InitialRState;
+          transitionTable[TerminateReal][period] = InitialRState;
+          transitionTable[TerminateReal][r_other] = InitialRState;
+     }
+
+
+     virtual bool processInput(string input, string &buffer) {
+          char currentChar;
+          for (int i = 0; i < input.size(); i++) {
+               if (currentState == TerminateReal || currentState == InvalidReal)
+                    break;
+               currentChar = input[i];
+               currentState = transitionTable[currentState][inputType(currentChar)];
+               if (currentState != InvalidReal && currentState != TerminateReal)
+                    buffer += currentChar;
+          }
+          //The state should be correctly set by the FSM
+          if (currentState != NumberWithFractionalPart && currentState != TerminateReal) {
                buffer = "";
                return false;
           }
           else
                return true;
+     }
+
+     virtual int inputType(char currentChar) {
+          if (isdigit(currentChar))
+               return r_digit;
+          else if (currentChar == '.')
+               return period;
+          else
+               return r_other;
      }
 };
